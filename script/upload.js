14a497f017817cf3a5a2f714311881a318ff9d28
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-07 12:39:08 +0100

    clear fixed the cico's issue

--- script/upload.js
+++ script/upload.js
@@ -131,15 +131,28 @@ let uploadedMessage = () => {
 }
 
 var Url2 = 'https://firebasestorage.googleapis.com/v0/b/spitfy-graphs.appspot.com/o/no-image.svg?alt=media&token=f5ff4fce-1d3d-4a84-9e44-75b7504e375a';
-
-document.addEventListener('change', () => {
+document.querySelector('#img-to-upload').addEventListener('change', () => {
     Url2 = document.querySelector('#img-to-upload').files[0]
+    let file = Url2
+
+    let ref = firebase.storage().ref();
+    let metadata = {
+        contentType: file.type
+    };
+    let task = ref.child('images/' + file.name).put(file, metadata);
+    task
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+            Url2 = url
+            console.log(Url2);
+
+        })
+
+
+
 })
 
 
-setTimeout(() => { console.log(Url2.name); }, 8000);
-
-console.log(typeof Url2);
 
 function fireStoreUpload() {
 
@@ -147,135 +160,113 @@ function fireStoreUpload() {
         e.preventDefault()
         // let imgUrls = ['https://firebasestorage.googleapis.com/v0/b/spitfy-graphs.appspot.com/o/no-image.svg?alt=media&token=f5ff4fce-1d3d-4a84-9e44-75b7504e375a'];
 
-        let file = Url2
-
-        console.log(typeof file);
 
 
-        if (typeof file == "object") {
-            console.log(file);
+        let date = new Date()
+        let year = date.getFullYear()
+        let month = date.getMonth() + 1
+        let day = date.getDay() + 2
+        let hour = date.getHours()
+        let min = date.getMinutes()
+        let sec = date.getSeconds()
 
-            let ref = firebase.storage().ref();
-            let metadata = {
-                contentType: file.type
-            };
-            let task = ref.child('images/' + file.name).put(file, metadata);
-            task
-                .then(snapshot => snapshot.ref.getDownloadURL())
-                .then(url => {
-                    Url2 = url
-                    console.log(Url2);
+        let extime = `${year} / ${month} / ${day} ${hour}: ${min}: ${sec}`
 
-                })
 
+        let firstname = document.querySelector('#firstname').value
+        let lastname = document.querySelector('#lastname').value
+        let email = document.querySelector('#email').value
+        let uAge = document.querySelector('#uAge').value
+
+        let mname = document.querySelector('#mname').value
+
+
+
+
+        let appearance = []
+        document.querySelectorAll(".appearance").forEach(tag => {
+            appearance.push(tag.value)
+        })
+        let ability = []
+        document.querySelectorAll(".ability").forEach(tag => {
+            ability.push(tag.value)
+        })
+        let creator = []
+        document.querySelectorAll(".creator").forEach(tag => {
+            creator.push(tag.value)
+        })
+        let extlinks = []
+        document.querySelectorAll(".extlinks").forEach(tag => {
+            extlinks.push(tag.value)
+        })
+
+
+        let height = document.querySelector('#height').value
+        let heightUnit = document.querySelector('#heightUnit').value
+        let weight = document.querySelector('#weight').value
+        let weightUnit = document.querySelector('#weightUnit').value
+        let age = document.querySelector('#age').value != "" ? document.querySelector('#age').value : ""
+
+        let dd = document.querySelector('#yyyy').value == "" ? `01` : document.querySelector('#dd').value
+        let mm = document.querySelector('#yyyy').value == "" ? `01` : document.querySelector('#mm').value
+        let yyyy = document.querySelector('#yyyy').value == "" ? `${birthDateCalc(age)}` : document.querySelector('#yyyy').value
+        let stringBirth = document.querySelector('#yyyy').value == "" ? `${birthDateCalc(age)} - 01 - 01` : `${yyyy} - ${mm} - ${dd}`
+
+
+        let ageCalcQ;
+        if (document.querySelector('#age').value != "" && document.querySelector('#yyyy').value == "") {
+            ageCalcQ = 'calculated';
+        } else {
+            ageCalcQ = 'notCalculated';
         }
 
-        setTimeout(() => {
-            let date = new Date()
-            let year = date.getFullYear()
-            let month = date.getMonth() + 1
-            let day = date.getDay() + 2
-            let hour = date.getHours()
-            let min = date.getMinutes()
-            let sec = date.getSeconds()
+        let pog = document.querySelector('#pog').value
+        let history = document.querySelector('#history').value
+        let about = document.querySelector('#about').value
 
-            let extime = `${year} / ${month} / ${day} ${hour}: ${min}: ${sec}`
-
-
-            let firstname = document.querySelector('#firstname').value
-            let lastname = document.querySelector('#lastname').value
-            let email = document.querySelector('#email').value
-            let uAge = document.querySelector('#uAge').value
-
-            let mname = document.querySelector('#mname').value
-
-
-
-
-            let appearance = []
-            document.querySelectorAll(".appearance").forEach(tag => {
-                appearance.push(tag.value)
-            })
-            let ability = []
-            document.querySelectorAll(".ability").forEach(tag => {
-                ability.push(tag.value)
-            })
-            let creator = []
-            document.querySelectorAll(".creator").forEach(tag => {
-                creator.push(tag.value)
-            })
-            let extlinks = []
-            document.querySelectorAll(".extlinks").forEach(tag => {
-                extlinks.push(tag.value)
-            })
-
-
-            let height = document.querySelector('#height').value
-            let heightUnit = document.querySelector('#heightUnit').value
-            let weight = document.querySelector('#weight').value
-            let weightUnit = document.querySelector('#weightUnit').value
-            let age = document.querySelector('#age').value != "" ? document.querySelector('#age').value : ""
-
-            let dd = document.querySelector('#yyyy').value == "" ? `01` : document.querySelector('#dd').value
-            let mm = document.querySelector('#yyyy').value == "" ? `01` : document.querySelector('#mm').value
-            let yyyy = document.querySelector('#yyyy').value == "" ? `${birthDateCalc(age)}` : document.querySelector('#yyyy').value
-            let stringBirth = document.querySelector('#yyyy').value == "" ? `${birthDateCalc(age)} - 01 - 01` : `${yyyy} - ${mm} - ${dd}`
-
-
-            let ageCalcQ;
-            if (document.querySelector('#age').value != "" && document.querySelector('#yyyy').value == "") {
-                ageCalcQ = 'calculated';
-            } else {
-                ageCalcQ = 'notCalculated';
-            }
-
-            let pog = document.querySelector('#pog').value
-            let history = document.querySelector('#history').value
-            let about = document.querySelector('#about').value
-
-            firebaseToUpload.doc().set(
-                {
-                    time: extime,
-                    user: {
-                        firstname: firstname,
-                        lastname: lastname,
-                        email: email,
-                        age: uAge,
+        firebaseToUpload.doc().set(
+            {
+                time: extime,
+                user: {
+                    firstname: firstname,
+                    lastname: lastname,
+                    email: email,
+                    age: uAge,
+                },
+                monster: {
+                    mname: mname,
+                    appearance: appearance,
+                    height: height,
+                    heightUnit: heightUnit,
+                    weight: weight,
+                    weightUnit: weightUnit,
+                    birth: {
+                        stringBirth: stringBirth,
+                        yyyy: yyyy,
+                        dd: dd,
+                        mm: mm,
+                        ageCalcQ: ageCalcQ
                     },
-                    monster: {
-                        mname: mname,
-                        appearance: appearance,
-                        height: height,
-                        heightUnit: heightUnit,
-                        weight: weight,
-                        weightUnit: weightUnit,
-                        birth: {
-                            stringBirth: stringBirth,
-                            yyyy: yyyy,
-                            dd: dd,
-                            mm: mm,
-                            ageCalcQ: ageCalcQ
-                        },
-                        pog: pog,
-                        ability: ability,
-                        creator: creator,
-                        history: history,
-                        about: about,
-                        extlinks: extlinks,
-                        images: Url2
+                    pog: pog,
+                    ability: ability,
+                    creator: creator,
+                    history: history,
+                    about: about,
+                    extlinks: extlinks,
+                    images: Url2
 
-                    }
                 }
-            )
+            }
+        )
 
-            document.querySelectorAll("input").forEach(element => {
-                element.value = "";
-            })
-            document.querySelectorAll("textarea").forEach(element => {
-                element.value = "";
-            })
-            uploadedMessage()
-        }, 3000);
+        document.querySelectorAll("input").forEach(element => {
+            element.value = "";
+        })
+        document.querySelectorAll("textarea").forEach(element => {
+            element.value = "";
+        })
+        uploadedMessage()
+
 
 
 
